f22e2169e755a5072af84c22c95bf8aa38923f7b
Christ Degni <145073440+c-degni@users.noreply.github.com>
2025-11-07 03:35:13 +0000

    Accepted: Runtime - 0 ms (100.00%), Memory - 9 MB (8.34%)

--- solutions/0058-length-of-last-word/solution.cpp
+++ solutions/0058-length-of-last-word/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            char tmp = s[i];
-            s[i] = s[n - i - 1];
-            s[n - i - 1] = tmp;
-        }
-        int c = 0;
         int i = 0;
-        while (!c && i < n) {
-            while (i < n && isalpha(s[i])) {
-                c++;
-                i++;
+        int n = s.size();
+        string lastWord = "";
+        while (i < n) {
+            if (isalpha(s[i])) {
+                lastWord = "";
+                while (i < n && isalpha(s[i])) {
+                    lastWord += s[i];
+                    i++;
+                }
             }
             i++;
         }
-        return c;
+        return lastWord.length();
     }
 };
