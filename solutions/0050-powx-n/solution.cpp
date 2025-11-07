e0e33eefdee5305872f8034fe44e58ae18b1cd69
Christ Degni <145073440+c-degni@users.noreply.github.com>
2025-11-07 04:58:08 +0000

    Sync LeetCode submission Runtime - 1896 ms (1.54%), Memory - 8.6 MB (43.73%)

---
+++ solutions/0050-powx-n/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (!n || x == 1 || x == -1 && n < 0) return 1;
+        if (x == -1 && n > 0) return -1;
+        double r = 1;
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                if (r * x >= numeric_limits<double>::max()) return 0.0;
+                r *= x;
+            }
+        } else {
+            unsigned int t = (unsigned int)((double)n * -1);
+            for (unsigned int i = 0; i < t; i++) {
+                if (x > 0 && r * x <= numeric_limits<double>::min()) return 0.0;
+                r *= (1 / x);
+            }
+        }
+        return r;
+    }
+};
