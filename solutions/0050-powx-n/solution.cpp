081524a0f4c5483d4da8c6101b9bc5ce6b745027
Christ Degni <145073440+c-degni@users.noreply.github.com>
2025-11-07 04:58:08 +0000

    Accepted: Runtime - 1896 ms (1.54%), Memory - 8.6 MB (43.73%)

--- solutions/0050-powx-n/solution.cpp
+++ solutions/0050-powx-n/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (!n || x == 1 || x == -1 && !(n % 2)) return 1;
-        if (x == -1 && n % 2) return -1;
+        if (!n || x == 1 || x == -1 && n < 0) return 1;
+        if (x == -1 && n > 0) return -1;
         double r = 1;
         if (n > 0) {
             for (int i = 0; i < n; i++) {
