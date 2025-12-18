5d06c2ca3174a88e52a6b523e51413057c09d53e
Christ Degni <145073440+c-degni@users.noreply.github.com>
2025-12-18 21:17:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (71.61%)

---
+++ solutions/0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0) return;
+        int i = 0;
+        while (i < m) {
+            if (nums1[i] > nums2[0]) {
+                int t = nums1[i];
+                nums1[i] = nums2[0];
+                nums2[0] = t;
+                sort(nums2.begin(), nums2.end());
+            } else {
+                i++;
+            }
+        }
+
+        for (int &j : nums2) {
+            nums1[i] = j;
+            i++;
+        }
+    }
+};
