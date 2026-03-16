8cbca42a6ae820af98bc19f09d6d9e2307bddcd8
Christ Degni <145073440+c-degni@users.noreply.github.com>
2026-03-16 11:28:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.7 MB (67.56%)

---
+++ solutions/0031-next-permutation/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) j--;
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
