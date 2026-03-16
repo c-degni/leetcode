ae9ec42f095f0c6ac9d470c58bfc53083bc8352f
Christ Degni <145073440+c-degni@users.noreply.github.com>
2026-03-16 01:14:30 +0000

    Sync LeetCode submission Runtime - 41 ms (88.98%), Memory - 29 MB (91.84%)

---
+++ solutions/0015-3sum/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int last = INT_MIN;
+        int n = nums.size();
+        vector<vector<int>> out;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) return out;
+            
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    out.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                }
+            }
+        }
+        return out;
+    }
+};
